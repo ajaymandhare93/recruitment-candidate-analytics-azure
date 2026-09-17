eef9bb84e78b64b327625bc7825063d1809d7566
ajaymandhare93 <ajaymandhare93@gmail.com>
2026-09-17 20:36:46 +0530

    Add source tables SQL documentation

--- sql/source_tables.sql
+++ sql/source_tables.sql
@@ -1,1 +1,34 @@
+-- Recruitment Candidate Analytics
+-- Source Database: RecruitmentDB
+-- Source Tables: 5
 
+USE RecruitmentDB;
+
+-- 1. Candidates
+-- Stores candidate master information.
+SELECT *
+FROM Candidates;
+
+
+-- 2. Applications
+-- Stores candidate job application information.
+SELECT *
+FROM Applications;
+
+
+-- 3. Jobs
+-- Stores job/requisition information.
+SELECT *
+FROM Jobs;
+
+
+-- 4. Interviews
+-- Stores interview details for candidates.
+SELECT *
+FROM Interviews;
+
+
+-- 5. Offers
+-- Stores job offer details for candidates.
+SELECT *
+FROM Offers;
