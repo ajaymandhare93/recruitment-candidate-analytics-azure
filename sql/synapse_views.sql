d95495663064a7cf64deaa7f4ba47f1e54b1c188
ajaymandhare93 <ajaymandhare93@gmail.com>
2026-09-17 20:38:42 +0530

    Add Synapse SQL views

--- sql/synapse_views.sql
+++ sql/synapse_views.sql
@@ -1,1 +1,42 @@
+-- Recruitment Candidate Analytics
+-- Synapse Serverless SQL Views
+-- Database: RecruitmentDB
 
+USE RecruitmentDB;
+
+-- =========================================================
+-- View 1: Candidate Application Details
+-- =========================================================
+
+CREATE OR ALTER VIEW vw_candidate_applications AS
+
+SELECT
+    c.candidate_id,
+    c.first_name,
+    c.last_name,
+    c.email,
+    f.application_id,
+    f.job_id,
+    f.application_status
+FROM OPENROWSET(
+    BULK 'https://recruitmentdatalake01.dfs.core.windows.net/curated/dim_candidate',
+    FORMAT = 'DELTA'
+) AS c
+INNER JOIN OPENROWSET(
+    BULK 'https://recruitmentdatalake01.dfs.core.windows.net/curated/fact_application',
+    FORMAT = 'DELTA'
+) AS f
+    ON c.candidate_id = f.candidate_id;
+
+
+-- =========================================================
+-- View 2: Application Status Summary
+-- =========================================================
+
+CREATE OR ALTER VIEW vw_application_summary AS
+
+SELECT
+    application_status,
+    COUNT(*) AS total_applications
+FROM vw_candidate_applications
+GROUP BY application_status;
